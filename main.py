599045b919545586044b6e1d5a4d2f0bbef68a4a
Filip Swat <filipaswat@gmail.com>
2017-10-05 02:16:50 -0500

    added shitty data validation, form is techincally fully functional (validation criteria need to be established)

--- main.py
+++ main.py
@@ -11,12 +11,44 @@ def user_form():
 
 @app.route("/", methods=["POST"])
 def validate():
+    error_count = 0
+
+    name_error = ""
+    password_error = ""
+    verify_error = ""
+    email_error = ""
+
     username = request.form["username"]
     password = request.form["password"]
     verify = request.form["verify"]
     email = request.form["email"]
 
-    return render_template("user-confirmation.html", username=username,
-    password=password, verify=verify, email=email)
+    if not username:
+        error_count += 1
+        name_error = "Please enter a valid username"
+
+    if not password:
+        error_count += 1
+        password_error = "Please enter a valid password"
+    else:
+        if password != verify:
+            error_count += 1
+            verify_error = "Passwords do not match"
+    
+    if email:
+        if "a" not in email:
+            error_count += 1
+            email_error = "Please enter a valid email"
+
+    if error_count == 0:
+        return render_template("user-confirmation.html",
+        username=username, password=password, verify=verify, email=email)
+    else:
+        return render_template("user-signup.html",
+        username=username, email=email,
+        name_error=name_error, password_error=password_error,
+        verify_error=verify_error, email_error=email_error)
+
+    
 
 app.run()
