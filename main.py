16e61d0816d9e9d45a903c225beafefd8a582852
Filip Swat <filipaswat@gmail.com>
2017-10-05 02:46:58 -0500

    added regex data validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template
 import cgi
+import re
 
 app = Flask(__name__)
 
@@ -18,25 +19,29 @@ def validate():
     verify_error = ""
     email_error = ""
 
+    valid_username = re.compile(r"^[\w]{3,20}$")
+    valid_password = re.compile(r"^[\w]{3,20}$")
+    valid_email = re.compile(r"^[\w]@[\w]\.[\w]")
+
     username = request.form["username"]
     password = request.form["password"]
     verify = request.form["verify"]
     email = request.form["email"]
 
-    if not username:
+    if not valid_username.match(username):
         error_count += 1
-        name_error = "Please enter a valid username"
+        name_error = "Please enter a valid username (3-20 characters, no spaces)"
 
-    if not password:
+    if not valid_password.match(password):
         error_count += 1
-        password_error = "Please enter a valid password"
+        password_error = "Please enter a valid password (3-20 characters, no spaces)"
     else:
         if password != verify:
             error_count += 1
             verify_error = "Passwords do not match"
     
     if email:
-        if "a" not in email:
+        if not valid_email.match(email):
             error_count += 1
             email_error = "Please enter a valid email"
 
